23907003ddc634546cba1acbff3c63f0c88fe256
Rafael Palma Almeida <rafaelpalmaalmeida@hotmail.com>
2023-06-21 11:28:49 -0300

    Entrega aula 05

---
+++ scrip.js
@@ -0,0 +1,16 @@
+let pessoa1 = {
+    nome: "Rafael",
+    idade: 35,
+    Masculino: true,
+    pet: "dog"
+}
+
+let pessoa2 = {
+    nome: "Lucilia",
+    idade: 31,
+    Masculino: false,
+    pet: "dog"
+}
+
+
+console.log(pessoa1.nome)
